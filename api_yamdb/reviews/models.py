23f353bed4bde15e086dc462a1c10b5b77cb4349
SVSergej <45273319+SVSergej@users.noreply.github.com>
2021-11-08 17:45:33 +0300

    models_migrate_mac

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
+class Users(AbstractUser):
     ROLE_CHOICES = (
         ('U', 'user'),
         ('M', 'moderator'),
@@ -19,3 +19,78 @@ class User(AbstractUser):
         choices=ROLE_CHOICES,
         default='U'
     )
+
+
+class Categories(models.Model):
+
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+
+        return self.name
+
+
+class Genres(models.Model):
+
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+
+        return self.name
+
+
+class Titles(models.Model):
+
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    description = models.CharField(max_length=200)
+    genre = models.ForeignKey(Genres, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Categories, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+
+        return self.name
+
+
+#  class GenresTitles(models.Model):
+#    genre = models.ForeignKey(Genres, on_delete=models.SET_NULL, null=True)
+#    title = models.ForeignKey(Titles, on_delete=models.CASCADE)
+
+#    def __str__(self):
+#        return f'{self.genre} {self.title}'
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Titles,
+        on_delete=models.CASCADE,
+        related_name='title_review',
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        Users,
+        on_delete=models.CASCADE,
+        related_name='author_review',
+    )
+    score = models.SmallIntegerField()  # if 1 <= score <= 10
+    pub_date = models.DateField('date published', auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Comments(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='review_comment',
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        Users,
+        on_delete=models.CASCADE,
+        related_name='author_comment'
+    )
+    pub_date = models.DateField('date published', auto_now_add=True)
+
+    def __str__(self):
+        return self.text
