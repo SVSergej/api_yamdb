8b3b067795e41fcc9d2000dba9ea54582c4a9d33
chebextreme <ridedh@yandex.ru>
2021-11-17 16:46:19 +0300

    Соединил с веткой Заяна, теперь 25 тестов проходит

--- api_yamdb/api/serializers/review.py
+++ api_yamdb/api/serializers/review.py
@@ -7,9 +7,6 @@ from reviews.models import Review
 class ReviewSerializer(serializers.ModelSerializer):
     title = SlugRelatedField(slug_field='title', read_only=True)
     score = SlugRelatedField(slug_field='score', read_only=True)
-    # author = serializers.SlugRelatedField(
-    #     read_only=True, slug_field='username'
-    # )
 
     def validate_score(self, score):
         message = 'Оценка может ставиться от 1 до 10'
